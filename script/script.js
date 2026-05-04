709f443524ff45804cfe16f33306b165b6a5661e
Anna Nunes <nunes.santos.anna@escola.pr.gov.br>
2026-05-04 11:10:05 -0300

    ok

---
+++ script/script.js
@@ -0,0 +1,8 @@
+function salvarCarros(event) {
+    event.preventDefault();
+
+    let titulo = document.getElementById('title').value;
+    let preco = document.getElementById('preco').value;
+    let marca = document.getElementById('marca').value;
+    let modelo = document.getElementById('modelo').value
+}
